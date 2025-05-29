c48fd594abaa7d3c6d1140b83d5acc980dcd2986
Tracy Tran <tratran.315@gmail.com>
2025-05-29 22:30:35 +0200

    Update avatar in About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -34,43 +34,12 @@ const Hero = () => {
         </div>
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <div className="h-full w-full">
-          <svg
-            viewBox="0 0 420 420"
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-full h-full"
-          >
-            <defs>
-              <mask id="blob-mask">
-                <path
-                  fill="white"
-                  d="M39.9,-63C52.2,-62.1,62.8,-52.2,65.9,-40.2C69,-28.2,64.6,-14.1,65.5,0.5C66.4,15.1,72.5,30.3,65.2,35C57.9,39.7,37.1,34.1,24,42.3C10.9,50.6,5.5,72.7,-3.5,78.8C-12.5,84.9,-25,74.9,-35.3,65C-45.7,55.2,-54,45.6,-58.6,34.8C-63.3,23.9,-64.5,12,-62.3,1.3C-60.1,-9.4,-54.5,-18.9,-49,-28.2C-43.5,-37.6,-37.9,-46.8,-29.8,-50.1C-21.6,-53.3,-10.8,-50.5,1.5,-53.2C13.9,-55.8,27.7,-63.9,39.9,-63Z"
-                  transform="translate(200 200) scale(2.5)"
-                />
-              </mask>
-              <clipPath id="blob-clip">
-                <path
-                  d="M39.9,-63C52.2,-62.1,62.8,-52.2,65.9,-40.2C69,-28.2,64.6,-14.1,65.5,0.5C66.4,15.1,72.5,30.3,65.2,35C57.9,39.7,37.1,34.1,24,42.3C10.9,50.6,5.5,72.7,-3.5,78.8C-12.5,84.9,-25,74.9,-35.3,65C-45.7,55.2,-54,45.6,-58.6,34.8C-63.3,23.9,-64.5,12,-62.3,1.3C-60.1,-9.4,-54.5,-18.9,-49,-28.2C-43.5,-37.6,-37.9,-46.8,-29.8,-50.1C-21.6,-53.3,-10.8,-50.5,1.5,-53.2C13.9,-55.8,27.7,-63.9,39.9,-63Z"
-                  transform="translate(200 200) scale(2.5)"
-                />
-              </clipPath>
-            </defs>
-            <rect
-              width="400"
-              height="400"
-              fill="#FBBF24"
-              clip-path="url(#blob-clip)"
-            />
-            <image
-              href="/Tracy.png"
-              x="0"
-              y="0"
-              width="400"
-              height="400"
-              mask="url(#blob-mask)"
-              className="object-cover"
-            />
-          </svg>
+        <div className="h-full w-full flex items-center justify-center p-4">
+          <img 
+            src="/Tracy.png" 
+            alt="Tracy Tran" 
+            className="h-auto w-auto max-h-[80%] max-w-[80%] object-contain"
+          />
         </div>
       </div>
     </div>
